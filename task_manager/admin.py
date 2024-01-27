141044d998d8c7b522041c356adce9a4819ec0aa
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 03:33:07 +0200

    Add models to the admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import TaskType, Position, Worker, Task
 
-# Register your models here.
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'position')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'deadline', 'is_completed', 'priority', 'task_type')
+    filter_horizontal = ('assignees',)
