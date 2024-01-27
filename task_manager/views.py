0e3ec2f1584e877168289485a517703b4771a4cb
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 03:58:30 +0200

    Add WorkerListView and TaskListView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from task_manager.models import Worker, Task
 
@@ -15,3 +16,43 @@ def index(request):
     }
 
     return render(request, "task_manager/index.html", context=context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        name = self.request.GET.get("name")
+        if name:
+            return queryset.filter(name__icontains=name)
+        return queryset
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = WorkerSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Worker.objects.all()
+        username = self.request.GET.get("username")
+        if username:
+            return queryset.filter(username__icontains=username)
+        return queryset
