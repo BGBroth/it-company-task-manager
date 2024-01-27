126a11774d60cd03dd0bfc4f247107952d1971a4
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 12:06:22 +0200

    Add new urls urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,15 +3,30 @@ from task_manager.views import index, WorkerListView, TaskListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path(
-        "workers/",
-        WorkerListView.as_view(),
-        name="worker-list",
+        "tasks/<int:pk>/toggle-assign/",
+        toggle_assign_to_task,
+        name="toggle-task-assign",
+    ),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path(
+        "workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"
+    ),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path(
+        "workers/<int:pk>/update/",
+        WorkerLicenseUpdateView.as_view(),
+        name="worker-update",
     ),
     path(
-        "tasks/",
-        TaskListView.as_view(),
-        name="task-list",
+        "workers/<int:pk>/delete/",
+        WorkerDeleteView.as_view(),
+        name="worker-delete",
     ),
 ]
 
