4078658012cc7135a892078dcaee2f11296933aa
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 04:03:02 +0200

    Add workers/ and tasks/ to urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from task_manager.views import index
+from task_manager.views import index, WorkerListView, TaskListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "workers/",
+        WorkerListView.as_view(),
+        name="worker-list",
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list",
+    ),
 ]
 
 app_name = "task_manager"
