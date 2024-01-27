0d8b1a2fb6321d329850a80abf4ac917509adb03
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 03:46:30 +0200

    Add task_manager/urls.py & connect to project urls.py & add index view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Worker, Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
