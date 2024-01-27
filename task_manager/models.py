99a0bb4fc57d61439d1b633b66ec50b83452e58e
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-27 03:19:19 +0200

    Add models for tasks, task types, positions, and workers

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(models.Model):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Urgent', 'Urgent'),
+        ('High', 'High'),
+        ('Mid', 'Mid'),
+        ('Low', 'Low'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
+
+    def __str__(self):
+        return self.name
